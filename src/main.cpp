9612c1e6df79aef8263b914552ed764cb5b74674
paja <janusch.patas@gmx.de>
2019-05-21 08:16:59 +0200

    Fix of depth buffer problem -> glClear(GL_DEPTH_BUFFER_BIT)

--- src/main.cpp
+++ src/main.cpp
@@ -137,8 +137,8 @@ int main()
 
         // render
         // ------
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         lightingShader.use();
         lightingShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
