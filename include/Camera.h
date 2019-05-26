0cdf6d4cd02553840bebaa927a9dfca3c06bfc84
paja <janusch.patas@gmx.de>
2019-05-26 13:10:46 +0200

    Quaternion-based Camera works

--- include/Camera.h
+++ include/Camera.h
@@ -8,121 +8,108 @@
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtx/quaternion.hpp>
 
-namespace Constants
-{
-    const float YAW = -90.f;
-    const float PITCH = 0.0f;
-    const float SPEED = 2.5f;
-    const float SENSITYVITY = 0.1f;
-    const float ZOOM = 45.0f;
-    const float ROLL = 0.0f;
-};
-
 class Camera
 {
-    private:
-     enum class ECamDir
-    {
-        forward,
-        backward,
-        left,
-        right
-    };
-
     public:
         Camera() = default;
         Camera(glm::vec3 position, glm::vec3 up, float yaw, float pitch) :
-            m_cameraPos{position}, m_worldUp{up} {};
+            camPos{position}, worldUp{up} {};
         ~Camera() = default;
         void ProcessInput(GLFWwindow *window, float deltaTime);
         glm::mat4 GetViewMatrix(void) {return viewMatrix;};
 
     private:
-        void updateView(void);
+        void updateView(float pitch, float yaw);
+        void rotateCamera(float angle, float x, float y, float z);
 
     private:
-        glm::vec3 m_cameraPos = glm::vec3(0.0f, 0.0f, 0.0f);
-        glm::vec3 m_cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
-        glm::vec3 m_cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
-        glm::vec3 m_cameraRight = glm::vec3(1.0f, 0.0f, 0.0f);
-        glm::vec3 m_worldUp = glm::vec3(0.0f, 1.0f, 0.0f);
+        glm::vec3 camPos{0.0f, 0.0f, 3.0f};
+        glm::vec3 camRight{1.0f, 0.0f, 0.0f};
+        glm::vec3 worldUp{0.0f, 1.0f, 0.0f};
+        glm::vec3 camView{0.0f, 0.0f, -1.0f};
+        glm::vec3 camUp = worldUp;
 
-
-        float m_yaw = Constants::YAW;
-        float m_roll = Constants::ROLL;
-        float m_pitch = Constants::PITCH;
-        float m_zoom = Constants::ZOOM;
-        float m_m_cameraPositionmentSpeed = Constants::SPEED;
-        float m_mouseSensitivity = Constants::SENSITYVITY; 
-
-        glm::mat4 viewMatrix = glm::lookAt(m_cameraPos, m_cameraPos + m_cameraFront, m_cameraUp);
+        glm::mat4 viewMatrix = glm::lookAt(camPos, camView, camUp);
         glm::quat cameraQuat = glm::toQuat(viewMatrix);
 };
 
-void Camera::updateView(void)
+void Camera::rotateCamera(float angle, float x, float y, float z)
 {
-    glm::quat quatPitch = glm::quat(glm::vec3(m_pitch, 0.0f, 0.0f)); 
-    glm::quat quatYaw = glm::quat(glm::vec3(0.0f, m_yaw, 0.0f)); 
-    
-    m_pitch = m_roll = m_yaw = 0.0f;
+    glm::quat qview, qaxis;    
 
-    cameraQuat =  quatYaw * quatPitch * cameraQuat * glm::conjugate(quatPitch) * glm::conjugate(quatYaw);
-    cameraQuat = glm::normalize(cameraQuat);
-    glm::mat4 rotate = glm::mat4_cast(cameraQuat);
+    qaxis.x = x * std::sin(angle * 0.5);
+    qaxis.y = y * std::sin(angle * 0.5);
+    qaxis.z = z * std::sin(angle * 0.5);
+    qaxis.w = std::cos(angle * 0.5);
 
-    m_cameraFront =  glm::conjugate(quatPitch) * m_cameraFront * quatYaw; 
-    glm::mat4 trans = glm::mat4(1.0f);
-    trans = glm::translate(trans, m_cameraPos);
+    qview.x = camView.x;
+    qview.y = camView.y;
+    qview.z = camView.z;
 
-    viewMatrix = rotate * trans;
+    qview = qaxis * qview * glm::conjugate(qaxis);
+
+    camView.x = qview.x;
+    camView.y = qview.y;
+    camView.z = qview.z;
+}
+
+void Camera::updateView(float pitch, float yaw)
+{
+    glm::vec3 axis = glm::normalize(glm::cross(camView + camPos, camUp));
+    rotateCamera(yaw, worldUp.x, worldUp.y, worldUp.z);
+    rotateCamera(pitch, axis.x, axis.y, axis.z);
+    camRight = glm::normalize(glm::cross(camView, worldUp));
+    camUp = glm::normalize(glm::cross(camRight, camView));
+    viewMatrix = glm::lookAt(camPos, camPos + camView, camUp);
 }
 
 void Camera::ProcessInput(GLFWwindow *window, float deltaTime)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
         glfwSetWindowShouldClose(window, true);
+        return;
+    }
 
     float cameraSpeed = 2.5 * deltaTime; 
-    
+    float yaw = 0.0f; 
+    float pitch = 0.0f;
+    float sensitivity = 5.0;
+
     // Translation
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
     {
-        m_yaw -= cameraSpeed;
+        yaw += cameraSpeed;
     }
     else if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
     {
-        m_yaw += cameraSpeed;
+        yaw -= cameraSpeed;
     }
     else if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
     {
-        m_pitch -= cameraSpeed;
+        pitch -= cameraSpeed;
     }
     else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) 
     {
-        m_pitch += cameraSpeed;
-    }
-    else 
-    {
-       m_yaw = m_pitch = 0.0f; 
+        pitch += cameraSpeed;
     }
 
 // Rotation
     if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
     {
-        m_cameraPos -= m_cameraFront * cameraSpeed;
+        camPos += camView * cameraSpeed * sensitivity;
     }
     else if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
     {
-        m_cameraPos += m_cameraFront * cameraSpeed;
+        camPos -= camView * cameraSpeed * sensitivity;
     }
     else if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
     {
-        m_cameraPos += m_cameraRight * cameraSpeed;
+        camPos -= camRight * cameraSpeed * sensitivity;
     }
     else if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
     {
-        m_cameraPos -= m_cameraRight * cameraSpeed;
+        camPos += camRight * cameraSpeed * sensitivity;
     }
-    m_cameraRight = glm::normalize(glm::cross(m_cameraFront, m_worldUp));
-    updateView();
+    updateView(pitch, yaw);
 }
